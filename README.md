6fd1e74a88b6e07edeb7f20e46b919d1ae61196b
arefin <fig@arefin.net>
2025-05-20 16:38:34 -0700

    Initialize repo and start step 00. Doesn't work yet due to bug in provided example code.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # go-ai-agent
-Code following Thorsten Ball's How to Build an Agent tutorial.
+Code following Thorsten Ball's How to Build an Agent tutorial at <https://ampcode.com/how-to-build-an-agent>.
+
+Note: To run the code you will need an Anthropic API key set in the environment variable ANTHROPIC_API_KEY.
---
+++ step-00-hello-claude/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/anthropics/anthropic-sdk-go"
+	"github.com/anthropics/anthropic-sdk-go/option"
+)
+
+func main() {
+	client := anthropic.NewClient(
+		option.WithAPIKey("my-anthropic-api-key"), // defaults to os.LookupEnv("ANTHROPIC_API_KEY")
+	)
+	message, err := client.Messages.New(context.TODO(), anthropic.MessageNewParams{
+		MaxTokens: 1024,
+		Messages: []anthropic.MessageParam{{
+			Content: []anthropic.ContentBlockParamUnion{{
+				OfRequestTextBlock: &anthropic.TextBlockParam{Text: "What is a quaternion?"},
+			}},
+			Role: anthropic.MessageParamRoleUser,
+		}},
+		Model: anthropic.ModelClaude3_7SonnetLatest,
+	})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("%+v\n", message.Content)
+}
